c213c54d6afe841e0bcc55e88f3ec12a9df8ed90
Denis <denis@gmail.ru>
2025-11-17 14:12:23 +0300

    Делаю services

---
+++ src/components/pages/home/components/Services/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles/index.module.css";
+
+export const Services = () => {
+  return (
+    <section className={styles.services}>
+      <div className={styles.container}></div>
+    </section>
+  );
+};
