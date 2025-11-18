421fb07b5a8248e8a3364babe5b855c2fcd5b832
Denis <denis@gmail.ru>
2025-11-18 15:07:02 +0300

    этап подготовки элементов для блока advantages

---
+++ src/components/ui/icons/WindowsIcon/index.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+
+export const WindowsIcon: FC = () => (
+  <svg viewBox="0 0 35 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M34 1H1V42H34V1Z" stroke="#2096FF" strokeWidth="2" />
+    <path d="M19 2H17V42H19V2Z" fill="#2096FF" />
+  </svg>
+);
