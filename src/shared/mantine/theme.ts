fd8d66c7fb2f0285933be9811f63da4163fddf2e
Denis <denis@gmail.ru>
2025-12-16 22:40:59 +0300

    добавил тему мантин

---
+++ src/shared/mantine/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  fontFamily: "Mulish, system-ui, sans-serif",
+
+  primaryColor: "brand",
+
+  defaultRadius: "md",
+
+  colors: {
+    brand: [
+      "#e7f3ff",
+      "#cfe5ff",
+      "#9fcaff",
+      "#6eafff",
+      "#4096ff",
+      "#2096ff",
+      "#1a7fe0",
+      "#1368b8",
+      "#0c5291",
+      "#043c6b",
+    ],
+  },
+});
